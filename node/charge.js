32020e3a989be2da562fff1e0c27c1f7b4396361
RobUS <Robinson8@orange.fr>
2026-08-04 20:46:34 +0200

    Update charge.js

--- node/charge.js
+++ node/charge.js
@@ -1,13 +1,37 @@
-// charge les fichiers de jeu dans le contexte courant, tels quels
+// Charge les fichiers de jeu dans la portee globale.
+// eval() ne convient pas : les const et let y restent enfermes.
 const fs = require('fs');
 const path = require('path');
+const vm = require('vm');
+
+const SRC = ['config', 'moteur', 'observation', 'cerveau',
+             'recompense', 'collecte', 'gradients', 'entrainement'];
 
 function chargeJeu(racine) {
-  const src = ['config', 'moteur', 'observation', 'cerveau',
-               'recompense', 'collecte', 'gradients', 'entrainement'];
   let code = '';
-  for (const f of src) code += fs.readFileSync(path.join(racine, 'js', f + '.js'), 'utf8') + '\n';
-  return code;
+  for (const f of SRC) {
+    const p = path.join(racine, 'js', f + '.js');
+    if (!fs.existsSync(p)) throw new Error('fichier manquant : ' + p);
+    code += fs.readFileSync(p, 'utf8') + '\n';
+  }
+
+  // on recopie aussi les noms utiles sur globalThis, pour que les
+  // modules CommonJS y accedent sans ambiguite
+  const noms = ['N_CACHE', 'N_MOUV', 'N_VISEE', 'PAS_VISEE', 'PAS_DECISION',
+                'CLES', 'creeCerveau', 'creeAgentIA', 'agitIA', 'propage',
+                'masques', 'tireSoftmax', 'creeRng', 'creePartie', 'pas',
+                'observe', 'tailleObservation', 'creeMemoire',
+                'creeSession', 'avanceSession', 'collecteFournee',
+                'statistiques', 'instantane', 'recompense', 'calculeAvantages',
+                'creeGradients', 'videGradients', 'retropropageSegment',
+                'perteSegment', 'verifieGradients',
+                'creeEntrainement', 'pasEntrainement', 'creeAdam',
+                'copieCerveau', 'exporteCerveau', 'importeCerveau'];
+
+  code += '\n;' + JSON.stringify(noms) +
+          '.forEach(function (n) { try { globalThis[n] = eval(n); } catch (e) {} });';
+
+  vm.runInThisContext(code, { filename: 'jeu.js' });
 }
 
 module.exports = { chargeJeu };
